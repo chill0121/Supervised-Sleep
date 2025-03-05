461c35b800600ea3d136dc1989e937ef323c4941
Cody Hill <104405809+chill0121@users.noreply.github.com>
2025-03-04 17:14:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import psycopg2
-from database.db_utilities import *#create_table, delete_table, table_list, DB_NAME, DB_USER, DB_HOST, DB_PORT
+from database.db_utilities import * #create_table, delete_table, table_list, DB_NAME, DB_USER, DB_HOST, DB_PORT
+from config.config import * #BASE_DIR, TOKEN_PATH, DATA_DIR, TODAY, TODAY_DATETIME
 
 def main():
     """Main function to initialize database and create tables."""
     try:
         connection = psycopg2.connect(database=DB_NAME, user=DB_USER, host=DB_HOST, port=DB_PORT)
         # Delete All for troubleshooting.
-        # for table in table_list:
-        #     delete_table(connection, [*table][0])
+        for table in table_list:
+            delete_table(connection, [*table][0])
         
         # Create All
         for table in table_list:
             create_table(connection, table)
 
-            
+        insert_to_db(connection, 1)
         connection.close()
 
     except psycopg2.Error as e:
