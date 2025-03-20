a72faee10f0473b8265eb09a7b1dd06c0606a42d
Cody Hill <104405809+chill0121@users.noreply.github.com>
2025-03-20 14:50:16 -0700

    Created materialized views.

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,4 @@
 from .db_utilities import create_table, delete_table, update_pending_data, insert_json_files_to_db
+from .materialized_views import initialize_materialized_views, refresh_views
 
-__all__ = ['create_table', 'delete_table', 'update_pending_data', 'insert_json_files_to_db']
+__all__ = ['create_table', 'delete_table', 'update_pending_data', 'insert_json_files_to_db', 'initialize_materialized_views', 'refresh_views']
