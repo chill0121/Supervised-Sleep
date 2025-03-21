160c910cfbffee68d4ef1faf6df4074e55377c2f
Cody Hill <104405809+chill0121@users.noreply.github.com>
2025-03-21 15:21:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import psycopg2
-from database.db_utilities import * #create_table, delete_table, table_list, DB_NAME, DB_USER, DB_HOST, DB_PORT
-from config.settings import * #BASE_DIR, TOKEN_PATH, DATA_DIR, TODAY, TODAY_DATETIME
+from database import db_utilities, views #create_table, delete_table, table_list
+from config import settings #BASE_DIR, TOKEN_PATH, DATA_DIR, TODAY, TODAY_DATETIME
 from oura_api.fetch_data import fetch_process_save_data, fetch_historical_data
 
 def main():
     """Main function to initialize database and create tables."""
     # fetch_historical_data()
     try:
-        # fetch_process_save_data()
-        connection = psycopg2.connect(database=DB_NAME, user=DB_USER, host=DB_HOST, port=DB_PORT)
+        fetch_process_save_data()
+
+        connection = psycopg2.connect(database=settings.DB_NAME, user=settings.DB_USER, host=settings.DB_HOST, port=settings.DB_PORT)
         # Delete All for troubleshooting.
-        for table in table_dict.keys():
-            delete_table(connection, table)
+        # for table in db_utilities.table_dict.keys():
+        #     db_utilities.delete_table(connection, table)
         
         # Create All
-        for table, columns in table_dict.items():
-            create_table(connection, {table: columns})
+        for table, columns in db_utilities.table_dict.items():
+            db_utilities.create_table(connection, {table: columns})
+        db_utilities.insert_json_files_to_db(connection, 1)
 
-        insert_json_files_to_db(connection, 1)
+        views.initialize_materialized_views(connection)
+
         connection.close()
 
     except psycopg2.Error as e:
