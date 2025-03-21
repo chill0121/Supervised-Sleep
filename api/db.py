459836b5468103bd6ddfbc032a8fe3f95f1130f7
Cody Hill <104405809+chill0121@users.noreply.github.com>
2025-03-21 15:09:58 -0700

    Create db.py - db to dashboard api functions.

---
+++ api/db.py
@@ -0,0 +1,24 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import settings
+
+# Database connection function
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=settings.DB_NAME,
+        user=settings.DB_USER,
+        # password=settings.DB_PASSWORD,
+        host=settings.DB_HOST,
+        port=settings.DB_PORT,
+        cursor_factory=RealDictCursor
+    )
+
+# Function to execute queries
+def fetch_query_results(query, params=None):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(query, params or ())
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return results
